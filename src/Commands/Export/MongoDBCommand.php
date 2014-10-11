52f455988c2210ccc190631bd0634bf294e2895a
buonzz <buonzz@gmail.com>
2014-10-11 21:38:14 +0800

    added exporter to mongodb

---
+++ src/Commands/Export/MongoDBCommand.php
@@ -0,0 +1,64 @@
+<?php namespace Buonzz\Scalp\Commands\Export;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Buonzz\Scalp\DirectoryParser;
+use Symfony\Component\Console\Logger\ConsoleLogger;
+use Buonzz\Scalp\ScalpLogger;
+use Symfony\Component\Console\Helper\ProgressBar;
+
+
+class MongoDBCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('export:mongodb')
+            ->setDescription('Accepts a folder then dump its structure into a script that can be used to store into MongoDB')
+            ->addOption(
+                'folder-path',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'What is the absolute path to the folder?',
+                '.'
+            )->addOption(
+                'output-file',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'What file to dump this?',
+                'mongodb_dump.js'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $text = '';
+        
+        $path = $input->getOption('folder-path');
+        $output_file = $input->getOption('output-file');
+        
+        $logger = new ConsoleLogger($output);
+        $slogger = new ScalpLogger($logger);
+
+        $progress = new ProgressBar($output);        
+
+        if (!file_exists($path)) {
+            $text = "Error: ".$path ." <-- cant find the specified folder";        
+            throw new \Exception($text);
+        }
+        else
+        {
+            $gen = new MongoDBScriptGenerator($path, 'media', 'pics');
+            $progress->start();            
+            $progress->advance();
+            $data_output = $gen->generate();
+            file_put_contents($output_file,$data_output);
+            $progress->finish();           
+        } 
+
+        $output->writeln("\nDump written on " . $output_file);               
+    }
+}
---
+++ src/Commands/Export/MongoDBScriptGenerator.php
@@ -0,0 +1,53 @@
+<?php namespace Buonzz\Scalp\Commands\Export;
+
+use Buonzz\Scalp\ExcludedContents;
+
+class MongoDBScriptGenerator{
+	private $path;
+	private $output;
+	private $db;
+	private $collection;
+
+	public function __construct($path, $db, $collection){
+		$this->path = $path;		
+		$this->db = $db;
+		$this->collection = $collection;
+	}
+
+	public function generate(){		
+		$this->output = "conn = new Mongo(); \r\n";
+		$this->output .= "db = conn.getDB('".$this->db."'); \r\n";
+		$this->output .= $this->array_to_script($this->path);
+		return $this->output;
+	}
+
+	private function array_to_script($path){		
+	   $result = ''; 
+       $cdir = scandir($path); 
+       foreach ($cdir as $key => $value) 
+       { 
+          if (!in_array($value,ExcludedContents::get())) 
+          { 
+
+             if (is_dir($path . DIRECTORY_SEPARATOR . $value)) 
+             { 
+                $result .= $this->array_to_script($dir . DIRECTORY_SEPARATOR . $value); 
+             } 
+             else 
+             { 
+                $result .= $this->get_meta($value, $path . DIRECTORY_SEPARATOR); 
+             } 
+          } 
+       } 
+           
+       return $result; 
+    }
+
+    private function get_meta($item, $path){
+    	$output = 'db.'.$this->collection.'.insert({';
+		$output .= '"file_name":"'. $item.'",';		
+		$output .= '"path":"'. $path.'"';
+		$output .=  "});\r\n";
+		return $output;
+    }
+}
