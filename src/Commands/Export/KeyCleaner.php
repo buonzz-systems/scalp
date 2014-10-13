9794fa12d8d8e94e714c5a5aa2e59506c87f0c0e
buonzz <buonzz@gmail.com>
2014-10-14 07:20:48 +0800

    tried using regex

--- src/Commands/Export/KeyCleaner.php
+++ src/Commands/Export/KeyCleaner.php
@@ -2,21 +2,9 @@
 
 class KeyCleaner{  
 	public static function clean($value){
-      $patterns = array();
-      
-      $patterns[0] = '/Thumbnail.FileType/';
-      $patterns[1] = '/Thumbnail.MimeType/';
-      $patterns[2] = '/\[Content_Types\]\.xml/';
-      $patterns[3] = '/\.rels/';      
-           
-
-      $replacements = array();
-      
-      $replacements[0] = 'Thumbnail-FileType';
-      $replacements[1] = 'Thumbnail-MimeType';
-      $replacements[2] = 'Content_Types_xml';
-      $replacements[3] = 'rels';
-
-      return preg_replace($patterns, $replacements, $value);     
+            return preg_replace_callback('/"(\w+\.[a-zA-Z]*)":/', function($matches){
+                  //return str_replace('.','_',$matches[0]);
+                  return 'hi';
+      }, $value);     
   }
 }
--- tests/Unit/Commands/Export/KeyCleanerTest.php
+++ tests/Unit/Commands/Export/KeyCleanerTest.php
@@ -11,10 +11,19 @@ class KeyCleanerTest extends PHPUnit_Framework_TestCase{
 
   public function testClean(){
       $var = new KeyCleaner;
-      $this->assertTrue(KeyCleaner::clean("Thumbnail.FileType") == 'Thumbnail-FileType');
-      $this->assertTrue(KeyCleaner::clean("Thumbnail.MimeType") == 'Thumbnail-MimeType');
+
+      echo KeyCleaner::clean("Thumbnail.FileType") ."\r\n";
+      echo KeyCleaner::clean("Thumbnail.MimeType") ."\r\n";
+      echo KeyCleaner::clean("[Content_Types].xml") ."\r\n";
+      echo KeyCleaner::clean(".rels");
+
+
+      $this->assertTrue(KeyCleaner::clean("Thumbnail.FileType") == 'Thumbnail_FileType');
+      $this->assertTrue(KeyCleaner::clean("Thumbnail.MimeType") == 'Thumbnail_MimeType');
       $this->assertTrue(KeyCleaner::clean("[Content_Types].xml") == 'Content_Types_xml');
-      $this->assertTrue(KeyCleaner::clean(".rels") == 'rels');
+      $this->assertTrue(KeyCleaner::clean(".rels") == '_rels');
+
+
       unset($var);
   }
 }
