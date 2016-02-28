e5bab6564e8eac7b0a09c7551e88627fdc024532
buonzz <buonzz@gmail.com>
2016-02-28 17:19:28 +0000

    reboot

--- src/Commands/Export/KeyCleaner.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Buonzz\Scalp\Commands\Export;
-
-class KeyCleaner{  
-	public static function clean($value){
-            return preg_replace_callback('/"(\S*\.[a-zA-Z]*)":/i', function($matches){
-                  return str_replace('.','_',$matches[0]);                  
-      }, $value);     
-  }
-}
--- src/Repositories/MongoDB/BaseContent.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Buonzz\Scalp\Repositories\MongoDB;
-
-use Buonzz\Scalp\Repositories\ContentInterface;
-
-class BaseContent implements ContentInterface{
-		
-}
