75c1fefd0ae011452561dcabc10bdd46bbbd7912
buonzz <buonzz@gmail.com>
2017-04-16 19:58:28 +0800

    moved common stuffs to base generator

---
+++ src/BaseGenerator.php
@@ -0,0 +1,34 @@
+<?php namespace Buonzz\Scalp;
+
+use Buonzz\Scalp\Analyzer;
+use Buonzz\Scalp\MediaFilesList;
+use Buonzz\Scalp\ExcludedContents;
+
+class BaseGenerator{
+
+	protected $input_folder;
+	protected $output_folder;
+	protected $output_file_list;
+	protected $files;
+	protected $analyzer;
+
+	public function __construct($input_folder, $output_folder, $output){
+		$this->input_folder = $input_folder;
+		$this->output_folder = $output_folder;
+		$this->output = $output;
+	}
+
+	public function generate(){
+
+		$this->analyzer = new Analyzer();
+		$this->files = MediaFilesList::get($this->input_folder);
+		$this->output_file_list = [];
+
+		if(file_exists($this->output_folder . '/'. "files.json"))
+        {
+            $this->output_file_list = json_decode(
+                file_get_contents($this->output_folder . '/'. "files.json"), true);
+        }
+	}
+
+}
--- src/MetadataGenerator.php
+++ src/MetadataGenerator.php
@@ -1,37 +1,20 @@
 <?php namespace Buonzz\Scalp;
 
-use Buonzz\Scalp\Analyzer;
-use Buonzz\Scalp\MediaFilesList;
-use Buonzz\Scalp\ExcludedContents;
-
-class MetadataGenerator{
-
-	private $input_folder;
-	private $output_folder;
+class MetadataGenerator extends BaseGenerator{
 
 	public function __construct($input_folder, $output_folder, $output){
-		$this->input_folder = $input_folder;
-		$this->output_folder = $output_folder;
-		$this->output = $output;
+        parent::__construct($input_folder, $output_folder, $output);
 	}
 
 	public function generate(){
 
-		$analyzer = new Analyzer();
-		$files = MediaFilesList::get($this->input_folder);
-		$output_file_list = [];
+        parent::generate();
 
-		if(file_exists($this->output_folder . '/'. "files.json"))
-        {
-            $output_file_list = json_decode(
-                file_get_contents($this->output_folder . '/'. "files.json"), true);
-        }
-
-        foreach($files as $k=>$file)
+        foreach($this->files as $k=>$file)
         {
             
             try {
-            	$data = $analyzer->analyze($file->getRealPath(),true);
+            	$data = $this->analyzer->analyze($file->getRealPath(),true);
             	$info = json_decode($data);
             	$filename = $info->file_contents_hash . ".json";
 	            $this->output->writeln('<comment>'. $filename .  '</comment> metadata extracted');
