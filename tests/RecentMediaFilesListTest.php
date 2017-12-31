f3131a643672d576fd4a85ef39386b86b7b3c2d8
buonzz <buonzz@gmail.com>
2017-12-31 13:21:27 +0800

    test sample

--- tests/RecentMediaFilesListTest.php
+++ tests/RecentMediaFilesListTest.php
@@ -3,9 +3,10 @@
 class RecentMediaFilesListTest extends PHPUnit_Framework_TestCase{
 	
   public function testSort(){
-    	$var = new Buonzz\Scalp\MediaFilesList;
-    	$this->assertTrue(is_object($var));
-    	unset($var);
+
+      $files = RecentMediaFilesList::get('/home/nfs/vol2/pics/12242017');
+      //    	$this->assertTrue(is_object($var));
+      var_dump($files);
   }
   
 }
